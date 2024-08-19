e63deadb40f44d8b3ed710b2f690f19d12c86058
jefflueck <lueckcfd@yahoo.com>
2024-08-19 11:08:55 -0500

    made a expenses_list.dart widget, made dummy data to display with expenses in expenses.dart file, and made a enum type Category with a list in expenses.dart in models folder

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -10,13 +13,29 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.00,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Column(
       children: [
-        Text('The Chart'),
-        Text('Expense List'),
+        const Text('The Chart'),
+        Expanded(
+          child: ExpensesList(
+            expenses: _registeredExpenses,
+          ),
+        )
       ],
     ));
   }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -3,16 +3,20 @@ import 'package:uuid/uuid.dart';
 // create a unique id for each expense with the help of the uuid package.
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   // Constructor
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
   // Properties
   final String id;
   final String title;
   final double amount; // 1.99
   final DateTime date;
+  final Category category;
 }
