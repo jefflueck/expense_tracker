2c9236a170219c4743ce19d81699c410622c7811
jefflueck <lueckcfd@yahoo.com>
2024-08-19 11:56:27 -0500

    added intl package from pub.dev, made icons dynamic based on category of expense in models/expense.dart, changed file structure, added getter to format date in combination with new intl package

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,9 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 // create a unique id for each expense with the help of the uuid package.
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.fastfood,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   // Constructor
@@ -19,4 +30,9 @@ class Expense {
   final double amount; // 1.99
   final DateTime date;
   final Category category;
+
+  // Methods
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
@@ -13,7 +14,7 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpenseItem(expenses[index]),
     );
   }
 }
