61c90068b4c83fd6e2d64c0b8671ea23f190a78e
jefflueck <lueckcfd@yahoo.com>
2024-09-03 09:25:03 -0500

    Added date and time with a variable to hold current date and time

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,18 +10,94 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+  // Alternative way to handle text input value of our form data.
+
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year, now.month - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
+  // void _saveInputValue(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
   @override
+  void dispose() {
+    _titleController.dispose();
+    _valueController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return const Padding(
-        padding: EdgeInsets.all(16),
+    return Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
             TextField(
+              controller: _titleController,
+              // onChanged: _saveInputValue,
               maxLength: 50,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 label: Text('Title'),
               ),
             ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _valueController,
+                    // onChanged: _saveInputValue,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      prefixText: '\$ ',
+                      label: Text('Value'),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text('Selected Date'),
+                      IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // print(_enteredTitle);
+                    print(_titleController.text);
+                    print(_valueController.text);
+                  },
+                  child: const Text('Add Expense'),
+                )
+              ],
+            )
           ],
         ));
   }
