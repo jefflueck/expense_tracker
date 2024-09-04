389cfa2948c4730673e0aa8cf4a53e981c2a1614
jefflueck <lueckcfd@yahoo.com>
2024-09-04 15:51:40 -0500

    added theme styling to the whole app in main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 75, 12, 220),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: Colors.red),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          centerTitle: true,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                  color: kColorScheme.onSecondaryContainer),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
